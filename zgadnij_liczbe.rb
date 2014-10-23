899851456f517ebbf25b46278dafee0c5844abd8
pietter <krzyzak51@o2.pl>
2014-10-23 13:17:39 +0200

    Update zgadnij_liczbe.rb

--- zgadnij_liczbe.rb
+++ zgadnij_liczbe.rb
@@ -1,21 +1,25 @@
-sekret =[rand(100)]
-#puts sekret
+los =[rand(100)]
+puts los
 print "zgadnij jaka liczbe mam na mysli (0-99)?   "
 while liczba = gets.chop!.to_i
 
-	if liczba == sekret.first
+	if liczba == los.first
+		system "clear"
 		puts "Wygrales!"
 		break
-	elsif liczba > sekret.first
+	elsif liczba > los.first
+		system "clear"
 		puts "Przykro mi, przegrales."
-		puts "Podana liczba jest za duza"
+		print "Podana liczba ", liczba, " jest za duza"
 	else
+		system "clear"
 		puts "Przykro mi, przegrales."
-		puts "Podana liczba jest za mala"
+		print "Podana liczba ", liczba, " jest za mala"
 		
 	end
+	puts
 	print "zgadnij o jakiej liczbie mysle (0-99)? "
 end
 
-puts "Chodzilo o ", sekret, "."
-
+print "Chodzilo o ", los.first, "."
+puts 
