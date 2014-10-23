e2946d22524d237517a8053ef484fb5784f55fd6
pietter <krzyzak51@o2.pl>
2014-10-23 12:57:15 +0200

    Create pierwiastek.rb

---
+++ pierwiastek.rb
@@ -0,0 +1,16 @@
+def pierwiastek(n)
+  if n < 0
+  system "clear"
+    puts "blad. Nie mozna liczyc pierwiastka z liczby ujemnej"
+  else
+    print "pierwiastek z ", n , " wynosi = ",Math.sqrt(n)
+  end
+end
+
+if ARGV.empty?
+	system "clear"
+	puts "podaj liczbe ktora chcesz pierwiastkowac jako argument"
+	exit
+end
+
+puts pierwiastek(ARGV[0].to_i)
